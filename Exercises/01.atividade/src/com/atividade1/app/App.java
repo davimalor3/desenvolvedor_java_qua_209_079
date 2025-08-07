ee3cdde7a9d78556b7817c7c8ef50b6b899dfc03
Davi <davi.nbtec@gmail.com>
2025-08-07 15:15:15 -0300

    Third commit, two java exercices

--- Exercises/01.atividade/src/com/atividade1/app/App.java
+++ Exercises/01.atividade/src/com/atividade1/app/App.java
@@ -26,14 +26,12 @@ public class App {
 
                 imc = peso/(height * height);
 
-                if (imc < 18.5) System.out.println("Abaixo do peso!\n"+name+", procure um nutricionista!");
-                else if(imc >= 18.5 || imc <= 24.9) System.out.println("Peso normal!");
-                else if(imc >= 25 || imc <= 29.9) System.out.println("Sobrepeso!");
-                else if(imc >= 30 || imc <= 34.9) System.out.println("Obesidade grau 1");
-                else if(imc >= 35 || imc <= 39.9) System.out.println("Obesidade grau 2");
+                if (imc < 18.5) System.out.println("\nAbaixo do peso!\n"+name+", procure um nutricionista!");
+                else if(imc >= 18.5 || imc <= 24.9) System.out.println("\nPeso normal!");
+                else if(imc >= 25 || imc <= 29.9) System.out.println("\nSobrepeso!\n"+name+", procure um nutricionista!");
+                else if(imc >= 30 || imc <= 34.9) System.out.println("\nObesidade grau 1\n"+name+", procure um nutricionista!");
+                else if(imc >= 35 || imc <= 39.9) System.out.println("\nObesidade grau 2\n"+name+", procure um nutricionista!");
                 else System.out.println("Obesidade grau 3!");
-
-                System.out.println("\n"+name+", procure um nutricionista!");
             } 
             else if (option !=2){
                 System.out.println("\n\n-------------------------\nOpção Invalida!\n-------------------------\n\n");
--- Exercises/02.atividade/src/com/atividade2/app/App.java
+++ Exercises/02.atividade/src/com/atividade2/app/App.java
@@ -5,52 +5,81 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         String name;
-        int age, option, salas;
-        boolean authorize = false;
-        
-        do{
+        int age, salas;
+        boolean ticket, authorize, quit = false;
+
             System.out.println("Informe seu nome:");
             name = sc.nextLine();
             System.out.println("Informe sua idade:");
             age = sc.nextInt();
             sc.nextLine();
-            
-            System.out.println("SALA 1 - RESIDENT EVIL (+18)");
-            System.out.println("SALA 2 - PEPPA PIG (LIVRE)");
-            System.out.println("SALA 3 - CORALINE (+7)");
-            System.out.println("SALA 4 - VELOZES FURIOSOS (+18)");
-            System.out.println("SALA 5 - HARRY POTTER (+12)");
+        
+        do{
+            // LISTA DE FILMES DISPONÍVEIS
+            System.out.println("\nESCOLHA O FILME PARA ASSISTIR:\n");
+            System.out.println("1 - RESIDENT EVIL (+18)");
+            System.out.println("2 - PEPPA PIG (LIVRE)");
+            System.out.println("3 - CORALINE (+7)");
+            System.out.println("4 - VELOZES FURIOSOS (+18)");
+            System.out.println("5 - HARRY POTTER (+12)");
             salas = sc.nextInt();
+            
+            //Garbage Collector
+            sc.nextLine();
 
-
+            
+            String movie = "";
+            ticket = false;
+            quit = false;
+            authorize = false;
+            
 
             switch(salas){
                 case 1:
+                    movie = "RESIDENT EVIL";
                     if (age >= 18) authorize = true;
                     break;
 
                 case 2:
+                    movie = "PEPPA PIG";
                     authorize = true;
                     break;
 
                 case 3:
+                    movie = "CORALINE";
                     if (age >= 7) authorize = true;
                     break;
 
                 case 4:
+                    movie = "VELOZES E FURIOSOS";
                     if (age >= 18) authorize = true;
                     break;
 
                 case 5:
+                    movie = "HARRY POTTER";
                     if (age >= 12) authorize = true;
                     break;
                     
                 default:
+                    System.out.println("Opção Invalida!");
+                    ticket = false;
+                    quit = true;
+
                     break;
             }
-
             
-        }while(option != 2 );
+            if (authorize)
+            {
+                System.out.println("\nOlá "+name+" o ingresso para o filme: "+movie+" está liberado!\nAproveite o filme!\n");
+                ticket = true;
+            }
+
+            else if(quit) System.out.println("Escolha novamente!");
+
+            else System.out.println("\n\nDesculpe "+name+", classificação indicativa não atingida\nEscolha outro filme de acordo com sua classificação compatível\n\n");
+            
+            
+        }while(!ticket);
 
 
         sc.close();

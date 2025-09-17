6c10c4e86803e174bc75249d16bc485d1f9ee378
Davi <davi.nbtec@gmail.com>
2025-09-17 17:45:23 -0300

    UPDATE - CRUD

--- Spring/CRUD System/CRUD-System/src/main/java/com/crud/app/controllers/CrudController.java
+++ Spring/CRUD System/CRUD-System/src/main/java/com/crud/app/controllers/CrudController.java
@@ -3,13 +3,18 @@ package com.crud.app.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.crud.app.models.Pessoa;
 import com.crud.app.repository.AppRepository;
+
 
 //Imports para paginação
 import org.springframework.data.domain.Page;
@@ -48,10 +53,15 @@ public class CrudController {
     //     mv.addObject("users", users);
     //     return mv;
     // }
+    // CRUD CONCEPT
+    @RequestMapping(value = "/about", method=RequestMethod.GET)
+    public String about() {
+        return "about";
+    }
 
-// ========== READ - CRUD (PAGINAÇÃO)
-@RequestMapping(value = "/search", method = RequestMethod.GET)
-public ModelAndView search(@RequestParam(defaultValue = "0") int page) {
+    // ========== READ - CRUD (PAGINAÇÃO)
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public ModelAndView search(@RequestParam(defaultValue = "0") int page) {
     int pageSize = 15;
 
     Pageable pageable = PageRequest.of(page, pageSize);
@@ -63,11 +73,37 @@ public ModelAndView search(@RequestParam(defaultValue = "0") int page) {
     mv.addObject("totalPages", users.getTotalPages());
     return mv;
 }
+    
+    
+    // UPDATE - CRUD
+    @RequestMapping(value = "/updateuser/{idPessoa}", method = RequestMethod.GET)
+    public ModelAndView updateUser(@PathVariable("idPessoa")long idPessoa){
+        Pessoa user = csr.findByIdPessoa(idPessoa);
+        ModelAndView mv = new ModelAndView("updateuser");
+        mv.addObject("user", user);
+        return mv;
+    }
 
-    // ABOUT CRUD 
-    @RequestMapping(value = "/about", method=RequestMethod.GET)
-    public String about() {
-        return "about";
+    @RequestMapping(value = "/updateuser/{idPessoa}", method=RequestMethod.POST)
+    public String updateUser(@Validated Pessoa user, BindingResult result, RedirectAttributes attributes) {
+        csr.save(user);
+        return "redirect:/updateuser";
+    }
+    
+    // DELETE - CRUD
+    @RequestMapping("/confirmDelete/{idPessoa}")
+    public ModelAndView confirmDelete(@PathVariable("idPesoa") long idPesoa){
+        Pessoa user= csr.findByIdPessoa(idPesoa);
+        ModelAndView mv = new ModelAndView("deleteUser");
+        mv.addObject("user", user);
+        return mv;
+    }
+
+    @RequestMapping("/deleteUser")
+    public String deleteUser(long idPesoa){
+        Pessoa user = csr.findByIdPessoa(idPesoa);
+        csr.delete(user);
+        return "redirect:/search";
     }
     
 }
--- Spring/CRUD System/CRUD-System/src/main/java/com/crud/app/repository/AppRepository.java
+++ Spring/CRUD System/CRUD-System/src/main/java/com/crud/app/repository/AppRepository.java
@@ -15,6 +15,8 @@ package com.crud.app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.crud.app.models.Pessoa;
 
+
 public interface AppRepository extends JpaRepository<Pessoa, Long> {
     Pessoa findByIdPessoa(long idPessoa);
+    Pessoa deleteByIdPessoa(long idPessoa);
 }

0574ade0a1e9103eb86af2e3deaf6583283919b5
Davi <davi.nbtec@gmail.com>
2025-09-22 17:25:18 -0300

    Modularization

---
+++ Spring/Modularizacao/src/main/java/com/modularizacao/app/controllers/ModuleController.java
@@ -0,0 +1,22 @@
+package com.modularizacao.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class ModuleController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/register")
+        public String register(){
+            return "register";
+
+        }
+    
+    
+}

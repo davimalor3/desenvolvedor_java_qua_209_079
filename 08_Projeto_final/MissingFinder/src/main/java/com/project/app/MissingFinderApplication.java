7667d45fc26d6c3a9adfbf1679cf7feaa666d9e0
Davi <davi.nbtec@gmail.com>
2025-09-23 17:45:30 -0300

    Final Project

---
+++ 08_Projeto_final/MissingFinder/src/main/java/com/project/app/MissingFinderApplication.java
@@ -0,0 +1,13 @@
+package com.project.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissingFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissingFinderApplication.class, args);
+	}
+
+}

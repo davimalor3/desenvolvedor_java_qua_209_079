b79c83b971d78d8557c4dc097aca1a10ad7a4111
Davi <davi.nbtec@gmail.com>
2025-10-02 17:43:17 -0300

    feat(entity): adiciona entidade User para substituir antiga estrutura de usuários

---
+++ 08_Projeto_final/MissingFinder/src/main/java/com/project/app/entities/User.java
@@ -0,0 +1,68 @@
+package com.project.app.entities;
+
+import jakarta.persistence.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.project.app.controllers.dto.LoginRequest;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "tb_user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+    
+
+    // ======== GETTERS AND SETTERS
+    public UUID getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return this.roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public boolean isLoginCorrect(LoginRequest loginRequest, PasswordEncoder passwordEncoder) {
+        return passwordEncoder.matches(loginRequest.password(), this.password);
+    }
+
+}

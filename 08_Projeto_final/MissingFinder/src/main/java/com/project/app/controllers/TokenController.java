e54a920316965f754ec737072ab5ce7b0dee9f1b
Davi <davi.nbtec@gmail.com>
2025-10-02 17:43:15 -0300

    feat(controller): implementa TokenController para geração e validação de JWT

---
+++ 08_Projeto_final/MissingFinder/src/main/java/com/project/app/controllers/TokenController.java
@@ -0,0 +1,69 @@
+package com.project.app.controllers;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.app.controllers.dto.LoginRequest;
+import com.project.app.controllers.dto.LoginResponse;
+import com.project.app.entities.Role;
+import com.project.app.repository.UserRepository;
+
+@RestController
+public class TokenController {
+
+    private final JwtEncoder jwtEncoder;
+    private final UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public TokenController(JwtEncoder jwtEncoder, UserRepository userRepository,
+            BCryptPasswordEncoder passwordEncoder) {
+        this.jwtEncoder = jwtEncoder;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+
+        var user = userRepository.findByUsername(loginRequest.username());
+
+        if (user.isEmpty() || !user.get().isLoginCorrect(loginRequest, passwordEncoder)) {
+            throw new BadCredentialsException("user or password is invalid!");
+        }
+
+        var now = Instant.now();
+        var expiresIn = 300L;
+
+                // Criação do token JWT
+        //scopes serve para guardar as roles(perfil: basico, admin) do usuario
+        var scopes = user.get().getRoles()
+                .stream()
+                .map(Role::getName)
+                .collect(Collectors.joining(" "));
+        
+        //claims = conteudo do token
+        var claims = JwtClaimsSet.builder()
+                .issuer("mybackend")
+                .subject(user.get().getUserId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .claim("scope", scopes)
+                .build();
+
+        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return ResponseEntity.ok(new LoginResponse(jwtValue, expiresIn));
+    }
+
+}

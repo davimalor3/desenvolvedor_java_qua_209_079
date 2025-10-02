f18920a8e063306d3a1acfddaef81daff1299724
Davi <davi.nbtec@gmail.com>
2025-10-02 17:43:06 -0300

    refactor(repository): ajusta interface de PessoaDesaparecidaRepository

--- 08_Projeto_final/MissingFinder/src/main/java/com/project/app/repository/PessoaDesaparecidaRepository.java
+++ 08_Projeto_final/MissingFinder/src/main/java/com/project/app/repository/PessoaDesaparecidaRepository.java
@@ -1,9 +1,13 @@
 package com.project.app.repository;
 
-import com.project.app.entities.PessoaDesaparecida;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
-public interface PessoaDesaparecidaRepository extends JpaRepository<PessoaDesaparecida, UUID> {
-    // PessoaDesaparecida findByIdPessoaDesaparecida(long idPessoaDesaparecidad);
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.app.entities.PessoaDesaparecida;
+
+@Repository
+public interface PessoaDesaparecidaRepository extends JpaRepository<PessoaDesaparecida, UUID>  {
+    //PessoaDesaparecida findByName(String nome);
 }
